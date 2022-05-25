e37ee59471631500998e5f9523649f90ec3762f7
Andres C <69328357+andres-c94@users.noreply.github.com>
2022-05-24 22:32:31 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+document.getElementById("carta").style.display="none"
+
+function Tarjeta(){
+    document.getElementById("tarjet").style.display="none"
+    document.getElementById("img1").style.display="none"
+    document.getElementById("carta").style.display="block"
+    document.getElementById("titlee").style.display="none"
+    
+}
+
